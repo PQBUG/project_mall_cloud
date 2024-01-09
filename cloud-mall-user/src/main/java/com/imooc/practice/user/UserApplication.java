fa5bcdacf68c81d64ad8913fbfab21c8c8ab0a53
PQBUG <hongyujia404@gmail.com>
2024-01-09 15:04:15 +0800

    user

--- cloud-mall-user/src/main/java/com/imooc/practice/user/UserApplication.java
+++ cloud-mall-user/src/main/java/com/imooc/practice/user/UserApplication.java
@@ -1,11 +1,15 @@
 package com.imooc.practice.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
+@MapperScan(basePackages = "com.imooc.practice.user.model.dao")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
